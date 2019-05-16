bc5c68b8967c1864c5ccac7bf28172dc49c14ecc
hustlion-dev <hustlion_dev@163.com>
2019-05-16 18:49:18 +0800

    not supporting rotation right now

--- lib/widgets_player.dart
+++ lib/widgets_player.dart
@@ -138,7 +138,7 @@ class _SVGAWidgetsTreeState extends State<SVGAWidgetsTree> {
     return Container(
       height: treeData.params.viewBoxHeight,
       width: treeData.params.viewBoxWidth,
-      color: m.Colors.green,
+//      color: m.Colors.green,
       child: drawShape(),
     );
   }
@@ -432,6 +432,27 @@ class _SVGAWidgetsTreeState extends State<SVGAWidgetsTree> {
       double top = 0;
       Matrix4 matrix4 = Matrix4.identity();
       Color fillColor;
+//      if (frameItem.hasTransform()) {
+//        var matrix = Float64List.fromList([
+//          frameItem.transform.a,
+//          frameItem.transform.b,
+//          0.0,
+//          0.0,
+//          frameItem.transform.c,
+//          frameItem.transform.d,
+//          0.0,
+//          0.0,
+//          0.0,
+//          0.0,
+//          1.0,
+//          0.0,
+//          frameItem.transform.tx,
+//          frameItem.transform.ty,
+//          0.0,
+//          1.0
+//        ].toList());
+//        matrix4 *= Matrix4.fromFloat64List(matrix);
+//      }
       frameItem.shapes.forEach((ShapeEntity shape) {
         final path = this.buildPath(shape);
         fillColor = colorFromFill(shape.styles.fill, frameItem.alpha);
@@ -462,10 +483,7 @@ class _SVGAWidgetsTreeState extends State<SVGAWidgetsTree> {
 //            0.0,
 //            1.0
 //          ].toList());
-//          container = m.Transform(
-//            transform: Matrix4.fromFloat64List(shapeMatrix),
-//            child: container,
-//          );
+//          matrix4 *= Matrix4.fromFloat64List(shapeMatrix);
         }
         if (frameItem.hasClipPath()) {
           // TODO get another clipPath
@@ -483,28 +501,6 @@ class _SVGAWidgetsTreeState extends State<SVGAWidgetsTree> {
         height *= frameItem.transform.d;
         left += frameItem.transform.tx / frameItem.transform.a;
         top += frameItem.transform.ty / frameItem.transform.d;
-//        var matrix = Float64List.fromList([
-//          frameItem.transform.a,
-//          frameItem.transform.b,
-//          0.0,
-//          0.0,
-//          frameItem.transform.c,
-//          frameItem.transform.d,
-//          0.0,
-//          0.0,
-//          0.0,
-//          0.0,
-//          1.0,
-//          0.0,
-//          frameItem.transform.tx,
-//          frameItem.transform.ty,
-//          0.0,
-//          1.0
-//        ].toList());
-//        finalStack = m.Transform(
-//          transform: Matrix4.fromFloat64List(matrix),
-//          child: finalStack,
-//        );
       }
       Widget container = Positioned(
         width: width * xScale,
@@ -516,8 +512,18 @@ class _SVGAWidgetsTreeState extends State<SVGAWidgetsTree> {
           child: Center(child: Text('This is layer ${sprite.imageKey} with flutter widgets.')),
         ),
       );
+//      Widget containerMatrix = m.Transform(
+//        transform: matrix4,
+//        child: Container(
+//          width: width * xScale,
+//          height: height * yScale,
+//          color: fillColor,
+//          child: Center(child: Text('This is layer ${sprite.imageKey} with flutter widgets.')),
+//        ),
+//      );
 //      print('painting frame ${currentFrame} for ${sprite.imageKey} and stats ${container}');
       theStackElements.add(container);
+//      theStackElements.add(containerMatrix);
     });
     finalStack = Stack(
       children: theStackElements,
